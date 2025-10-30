7c7a7370400bf21ffce30d01646c4feb6e4101bd
LeeAnnJ <lee_ann.j@qq.com>
2025-10-30 22:08:19 +0800

    update code

--- code/evaluations/baseline_scripts.py
+++ code/evaluations/baseline_scripts.py
@@ -1,5 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-
+import tools.io_utils as io_utils
 
 class ChatUniTestRunner():
     def __init__(self):
@@ -7,10 +10,32 @@ class ChatUniTestRunner():
 
 
 class UTGenRunner():
-    def __init__(self):
-        pass
+    data_folder = ""
 
+    def __init__(self, dfolder):
+        self.data_folder = dfolder
+        return
+    
+    def prepare_dataset(self, dataset):
+        csv_file = f"{self.data_folder}/projects_binary/classes.csv"
+        csv_header = ["project", "class"]
+        csv_data = []
+        extracted_classes = set()
+        for pname,pinfo in dataset.items():
+            for minfo in pinfo["focal-methods"]:
+                class_name = minfo["class"]
+                if class_name not in extracted_classes:
+                    csv_data.append([pname, class_name])
+                    extracted_classes.add(class_name)
+        io_utils.write_csv(csv_file, csv_data, csv_header)
+        pass
 
 if __name__ == "__main__":
     # test runners
+    from settings import BaseLine, FileStructure
+    utgen_data_folder = BaseLine.UTGEN_DATA
+    dataset_file = f"{FileStructure.DATASET_PATH}/dataset_info.json"
+    dataset = io_utils.load_json(dataset_file)
+    utgen_runner = UTGenRunner(utgen_data_folder)
+    utgen_runner.prepare_dataset(dataset)
     pass
---
+++ data/UTGen_replication/README.md
@@ -0,0 +1,26 @@
+# UTGen Replication for HITS Dataset
+This is a replication of the UTGen for the HITS dataset. Source code avaliable: https://github.com/amirdeljouyi/UTGen
+
+<!-- todo: check these contents -->
+## Requirements
+Docker or
+```c++
+Requirements = (Ollama && Java>=11 && Python>=3.10? && LLM_Interface)
+LLM_Interface = Nvidia tookits if you want to run model localy else Ollama_API
+```
+you can install requirements following these links:
+- [Docker](https://docs.docker.com/engine/install/)
+- [Ollama](https://ollama.ai/docs/install)
+- [Nvidia tookits](https://developer.nvidia.com/cuda-downloads)
+
+## Setup and Running Experiments:
+### Using Docker
+### Config Environment Manually
+### Scripts for generating test classes
+
+## Help: How to runing UTGen for my own dataset?
+config `classes.csv`
+
+### Config evosuite files for your projects
+
+### Update UTGen for a newer version of requirements
